c6fe59495d4ff51eb6efe6cac1b15d340495dd0e
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-05-04 14:02:24 +0200

    done

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-// TODO: load words (https://github.com/davidak/wortliste/blob/master/wortliste.txt) into efficient store
-// TODO: search words for syllable, return at least 7
+function search(syllable) {
+  let r = [];
+  for (let i = 0; i < wordList.length && r.length <= 5; i++) {
+    let w = wordList[i].toUpperCase();
+    if (w.includes(syllable)) {
+      r.push(w);
+    }
+  }
+  return r;
+}
 
 window.addEventListener("load", () => {
   const syllable = document.getElementById("syllable");
   const syllableDisplay = document.getElementById("syllable-display");
+  const matchesContainer = document.getElementById("matches-container");
   syllable.addEventListener("input", (e) => {
-    let syllable = e.target?.value;
+    let syllable = e.target?.value.toUpperCase();
     syllableDisplay.innerText = syllable;
+    let r = search(syllable).map((e) => {
+      let r = document.createElement("p");
+      r.innerText = e;
+      return r;
+    });
+    matchesContainer.replaceChildren(...r);
   });
 });
